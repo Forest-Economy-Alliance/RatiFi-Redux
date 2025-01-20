8544ed1a975fa1a88ab4c57f8a30de2c8d20767f
Siddharth Kumar Yadav <siddharthsk1234@gmail.com>
2025-01-20 17:29:03 +0530

    fix:claim-sync

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,2 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+â•ß€y!{›0´cßC®ã°.LTÃaÐmš„ûÅ—'âÏx°y˜6rÍF>EH9œf‘ì©Œ*•Ùü6'û- U,nîYBè>;ÜXUcq0ãÙ`^JfUS4•––Cw[ÐOvJR›	æõ7ð>Â il#_wÀfPsJ‚øŽbŽ V‰<®Ê ƒôæ1,¹¬zâò?âTÖP|ïÑ7šš¶¨T+¾²ÚøÝ7,B{Û·¥ê€RlîLS‘¡•º¨Å1ìXˆu<=N´LO»h Ä÷f(ŸˆÖ#˜ºf~aXÝ«,Ýÿ@Ž¾Qˆqj;ìº²ä`Ënj-šr	a´A_ÖûÐù
+„lv
